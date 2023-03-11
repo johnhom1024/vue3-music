5199f087b6bb6f85c4cdbc2a815497d8021454e0
anthony <924358746@qq.com>
2023-03-11 21:12:31 +0800

    initial file

---
+++ .eslintrc.cjs
@@ -0,0 +1,9 @@
+/* eslint-env node */
+require('@rushstack/eslint-patch/modern-module-resolution');
+
+module.exports = {
+  extends: ['@johnhom'],
+  rules: {
+    'vue/multi-word-component-names': 'off',
+  }
+};
